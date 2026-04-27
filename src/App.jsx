a042c27cfe8a23580044fdb5587ab3a2ea031626
crisostomo_e <crisostomo_e@ca01.ub.lan>
2026-04-27 12:44:53 +0800

    added react dom, page functions and navigation bar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,28 @@
 import { useState } from 'react'
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
 import './styles/App.css'
+
+function App() {
+  const location = useLocation();
+
+  useEffect(()=>{
+    switch (location.pathname) {
+      case '/admin': document.title = 'UB | Admin'; break;
+      case '/management': document.title = 'UB | Product Management'; break;
+      case '/return': document.title = 'UB | Returns & Logistics'; break;
+      default: document.title = 'UB';
+    }
+  }, [location]);
+
+  return (
+    <div className = "root-container"
+    <NavigationBar>
+    
+    <main className = 'root_page-container'>
+
+    </main>
+
+    </NavigationBar>
+  )
+}
